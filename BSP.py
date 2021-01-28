98b251013a6ca3d3760755d8a58cf494d3130949
R1chardJam3s <richard16j@gmail.com>
2021-01-28 19:22:22 +0000

    generate() method now works recursively
    BSP is done recursively, no lists are stored.

--- BSP.py
+++ BSP.py
@@ -2,59 +2,50 @@ import random, math
 from Partition import *
 
 class BSP:
-    def __init__(self, width, height, split_recursion):
+    def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.split_recursion = split_recursion
+        self.root = Partition_n(0, 0, self.width, self.height)
 
-    def generate(self):
-        partitions = []
-        partitions.append(Partition_n(0, 0, self.width, self.height))
-
-        for split in range(0, self.split_recursion):
-            temp_partitions = []
-            for partition in partitions:
-                if partition.splitable:
-                    if partition.width > partition.height * 1.5:
-                        split = 0
-                    elif partition.height > partition.width * 1.5:
-                        split = 1
-                    else:
-                        split = random.randint(0,1) #0 is x-axis, 1 is y-axis
-                    
-                    if split == 0:
-                        partition_start_x = partition.x
-                        partition_end_x = partition.x + partition.width
-                        partition_split = random.randint(partition_start_x + math.floor(partition.width / 4), partition_end_x - math.floor(partition.width / 4))
-                        temp_w = partition_split - partition_start_x
-                        if temp_w <= 250 and (temp_w < partition.height * 1.5 and temp_w * 1.5 > partition.height):
-                            temp_partitions.append(Partition_n(partition_start_x, partition.y, temp_w, partition.height, splitable=False))
-                        else:
-                            temp_partitions.append(Partition_n(partition_start_x, partition.y, temp_w, partition.height))
-                        temp_w = partition_end_x - partition_split
-                        if temp_w <= 250 and (temp_w < partition.height * 1.5 and temp_w * 1.5 > partition.height):
-                            temp_partitions.append(Partition_n((partition_split), partition.y, temp_w, partition.height, splitable=False))
-                        else:
-                            temp_partitions.append(Partition_n((partition_split), partition.y, temp_w, partition.height))
-                    else:
-                        partition_start_y = partition.y
-                        partition_end_y = partition.y + partition.height
-                        partition_split = random.randint(partition_start_y + math.floor(partition.height / 4), partition_end_y - math.floor(partition.height / 4))
-                        temp_h = partition_split - partition_start_y
-                        if temp_h <= 250 and (temp_h < partition.width * 1.5 and temp_h * 1.5 > partition.width):
-                            temp_partitions.append(Partition_n(partition.x, partition.y, partition.width, temp_h, splitable=False))
-                        else:
-                            temp_partitions.append(Partition_n(partition.x, partition.y, partition.width, temp_h))
-                        temp_h = partition_end_y - partition_split
-                        if temp_h <= 250 and (temp_h < partition.width * 1.5 and temp_h * 1.5 > partition.width):
-                           temp_partitions.append(Partition_n(partition.x, (partition_split), partition.width, temp_h, splitable=False))
-                        else:
-                            temp_partitions.append(Partition_n(partition.x, (partition_split), partition.width, temp_h))
+    def generate(self, partition=None):
+        if partition == None:
+            partition = self.root
+        if partition.largeEnough():
+            if partition.width/partition.height > 1.5:
+                split = 0
+            elif partition.height/partition.width > 1.5:
+                split = 1
+            else:
+                split = random.randint(0,1)
+            if split == 0:
+                partition_end = partition.x + partition.width
+                partition_split = random.randint(partition.x + math.floor(partition.width / 4), partition_end - math.floor(partition.width / 4))
+                temp_width = partition_split - partition.x
+                if temp_width < 250 and (temp_width/partition.height < 1.5 and partition.height/temp_width < 1.5):
+                    temp_partition_1 = Partition_n(partition.x, partition.y, temp_width, partition.height, splitable=False)
                 else:
-                    temp_partitions.append(partition)
-                #partition.splitable = False
-            partitions.clear()
-            for i in range(0, len(temp_partitions)):
-                partitions.append(temp_partitions[i])
-            temp_partitions.clear()
-        return partitions
+                    temp_partition_1 = Partition_n(partition.x, partition.y, temp_width, partition.height)
+                temp_width = partition_end - partition_split
+                if temp_width < 250 and (temp_width/partition.height < 1.5 and partition.height/temp_width < 1.5):
+                    temp_partition_2 = Partition_n(partition_split, partition.y, temp_width, partition.height, splitable=False)
+                else:
+                    temp_partition_2 = Partition_n(partition_split, partition.y, temp_width, partition.height)
+            else:
+                partition_end = partition.y + partition.height
+                partition_split = random.randint(partition.y + math.floor(partition.height / 4), partition_end - math.floor(partition.height / 4))
+                temp_height = partition_split - partition.y
+                if temp_height < 250 and (temp_height/partition.width < 1.5 and partition.width/temp_height < 1.5):
+                    temp_partition_1 = Partition_n(partition.x, partition.y, partition.width, temp_height, splitable=False)
+                else:
+                    temp_partition_1 = Partition_n(partition.x, partition.y, partition.width, temp_height)
+                temp_height = partition_end - partition_split
+                if temp_height < 250 and (temp_height/partition.width < 1.5 and partition.width/temp_height < 1.5):
+                    temp_partition_2 = Partition_n(partition.x, partition_split, partition.width, temp_height, splitable=False)
+                else:
+                    temp_partition_2 = Partition_n(partition.x, partition_split, partition.width, temp_height)
+            partition.setLeft(temp_partition_1)
+            partition.setRight(temp_partition_2)
+            if partition.left.splitable:
+                self.generate(partition=partition.left)
+            if partition.right.splitable:
+                self.generate(partition=partition.right)
