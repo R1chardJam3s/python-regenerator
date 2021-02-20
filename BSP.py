c43621547ef87a37c7afd46da66a8e807b5d2638
R1chardJam3s <richard16j@gmail.com>
2021-02-20 18:05:35 +0000

    Updated BSP.py
    Now can somewhat reliably calculate overlapping corridors to the selected room. New method at bottom but doesn't work. Still experimenting.

--- BSP.py
+++ BSP.py
@@ -74,14 +74,14 @@ class BSP:
             self.createCorridors(partition.right)
 
     def getPartition(self, x, y, partition=None):
-        print("run")
+        #print("run")
         if partition == None:
             partition = self.root
         if partition.hasRoom():
-            #if (partition.room.x <= x and (partition.room.x + partition.room.width) >= x) and (partition.room.y <= y and (partition.room.y + partition.room.height) >= y):
-            print(partition.hasRoom(), partition.isLeaf(), partition.value())
-            print(partition)
+            #print(partition.hasRoom(), partition.isLeaf(), partition.value())
+            #print(partition)
             self.base = partition
+            self.corridors = self.baseCorridors()
         elif not partition.largeEnough():
             print("Invalid Regeneration Location")
         elif partition.left.x == partition.right.x:
@@ -94,3 +94,63 @@ class BSP:
                 self.getPartition(x, y, partition=partition.left)
             else:
                 self.getPartition(x, y, partition=partition.right)
+
+    def baseCorridors(self):
+        temp_corridors = []
+        for corridor in self.corridors:
+            if corridor.start_y == corridor.end_y:
+                if (corridor.start_y + 4) >= self.base.room.y and (corridor.start_y - 4) <= (self.base.room.y + self.base.room.width):
+                    if corridor.start_x >= self.base.room.x and corridor.start_x <= (self.base.room.x + self.base.room.width):
+                        print("Corridor",corridor.getStart(), corridor.getEnd(),"is a base corridor")
+                        temp_corridors.append(corridor)
+                    elif corridor.end_x >= self.base.room.x and corridor.end_x <= (self.base.room.x + self.base.room.width):
+                        print("Corridor",corridor.getStart(), corridor.getEnd(),"is a base corridor")
+                        temp_corridors.append(corridor)
+                    elif self.base.room.x > corridor.start_x and self.base.room.x < corridor.end_x:
+                        print("Corridor",corridor.getStart(), corridor.getEnd(),"is a base corridor")
+                        temp_corridors.append(corridor)
+            else:
+                if (corridor.start_x + 4) >= self.base.room.x and (corridor.start_x - 4) <= (self.base.room.x + self.base.room.height):
+                    if corridor.start_y >= self.base.room.y and corridor.start_y <= (self.base.room.y + self.base.room.height):
+                        print("Corridor",corridor.getStart(), corridor.getEnd(),"is a base corridor")
+                        temp_corridors.append(corridor)
+                    elif corridor.end_y >= self.base.room.y and corridor.end_y <= (self.base.room.y + self.base.room.height):
+                        print("Corridor",corridor.getStart(), corridor.getEnd(),"is a base corridor")
+                        temp_corridors.append(corridor)
+                    elif self.base.room.y > corridor.start_y and self.base.room.y < corridor.end_y:
+                        print("Corridor",corridor.getStart(), corridor.getEnd(),"is a base corridor")
+                        temp_corridors.append(corridor)
+        return temp_corridors
+
+    def baseCorridors_new(self):
+        temp_corridors = []
+        r1 = (self.base.room.x, self.base.room.y)
+        r2 = (self.base.room.x + self.base.room.width, self.base.room.y + self.base.room.height)
+        print(r1,r2)
+        for corridor in self.corridors:
+            if corridor.start_y == corridor.end_y: #if horizontal
+                c1 = (corridor.start_x, corridor.start_y - 4)
+                c2 = (corridor.end_x, corridor.end_y + 4)
+
+                if self.overlap(*r1, *r2, *c1, *c2):
+                    print("true")
+                    temp_corridors.append(corridor)
+            else:
+                c1 = (corridor.start_x - 4, corridor.start_y)
+                c2 = (corridor.end_x + 4, corridor.end_y)
+
+                if self.overlap(*r1, *r2, *c1, *c2):
+                    print("true")
+                    temp_corridors.append(corridor)
+
+        return temp_corridors
+
+    #algorithm based on geeksforgeeks article: find two retangles overlap
+    def overlap(self, r1_x, r1_y, r2_x, r2_y, c1_x, c1_y, c2_x, c2_y):
+        if r1_x >= c2_x or c1_x >= r2_x:
+            return False
+
+        if r1_y <= c2_y or c1_y <= r2_y:
+            return False
+
+        return True
